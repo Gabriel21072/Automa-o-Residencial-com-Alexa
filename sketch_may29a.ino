f26a5f2709656f5acfcd773c0693460c96fd88f2
Gabriel21072 <67654049+Gabriel21072@users.noreply.github.com>
2020-06-30 15:16:23 -0300

    Add files via upload

---
+++ sketch_may29a.ino
@@ -0,0 +1,100 @@
+#include <ESP8266WiFi.h>   
+#include "SinricPro.h"
+#include "SinricProSwitch.h"
+ 
+#define WIFI_SSID         "damascenonet2"    
+#define WIFI_PASS         "multilaser1973"
+#define APP_KEY           "ee9de6ed-13dd-4954-a5bd-35294a1a3a15"      // O seu App Key é algo como "de0bxxxx-1x3x-4x3x-ax2x-5dabxxxxxxxx"
+#define APP_SECRET        "54d9af05-1fee-4b9f-bc73-30e9389e2681-308083e0-837f-444e-89cf-61d687a626c9"   // O seu App Secret é algo como "5f36xxxx-x3x7-4x3x-xexe-e86724a9xxxx-4c4axxxx-3x3x-x5xe-x9x3-333d65xxxxxx"
+ 
+#define Abajour_ID       "5ed12c5a3045f14787e485a7"    // Tomada 1
+#define Lampada_ID       "5ed12bf576b86a477f5e458b"    // Tomada 2
+#define Regua_ID         "5ed12c453045f14787e485a4"    // Tomada 3
+#define Ventilador_ID    "5ed12c7876b86a477f5e458f"    // Tomada 4
+ 
+ 
+#define Abajour_Pin 0  // O pino fisico onde está ligado
+#define Lampada_Pin 1  // O pino fisico onde está ligado
+#define Regua_Pin 2  // O pino fisico onde está ligado
+#define Ventilador_Pin 3  // O pino fisico onde está ligado
+ 
+#define BAUD_RATE         9600                // Se precisar, pode trocar o baud rate
+ 
+ 
+void setupWiFi();
+void setupSinricPro();
+bool AbajourState(const String &deviceId, bool &state);
+bool LampadaState(const String &deviceId, bool &state);
+bool ReguaState(const String &deviceId, bool &state);
+bool VentiladorState(const String &deviceId, bool &state);
+ 
+ 
+// main setup function
+void setup() {
+  setupWiFi();
+  setupSinricPro(); 
+  pinMode(Abajour_Pin, OUTPUT);
+  pinMode(Lampada_Pin, OUTPUT);
+  pinMode(Regua_Pin, OUTPUT);
+  pinMode(Ventilador_Pin, OUTPUT);
+  
+  digitalWrite(Abajour_Pin, HIGH);
+  digitalWrite(Lampada_Pin, HIGH);
+  digitalWrite(Regua_Pin, HIGH);
+  digitalWrite(Ventilador_Pin, HIGH);
+}
+ 
+void loop() {
+  SinricPro.handle();
+}
+ 
+ 
+bool AbajourState(const String &deviceId, bool &state) {
+  digitalWrite(Abajour_Pin, !state);
+  return true; // request handled properly
+}
+ 
+bool LampadaState(const String &deviceId, bool &state) {
+  digitalWrite(Lampada_Pin, !state);
+  return true; // request handled properly
+}
+ 
+bool ReguaState(const String &deviceId, bool &state) {
+  digitalWrite(Regua_Pin, !state);
+  return true; // request handled properly
+}
+ 
+bool VentiladorState(const String &deviceId, bool &state) {
+  digitalWrite(Ventilador_Pin, !state);
+  return true; // request handled properly
+}
+ 
+// setup das conexões Wifi
+void setupWiFi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(250);
+  }
+}
+ 
+// setup das funções para o SinricPro
+void setupSinricPro() {
+  // add devices and callbacks to SinricPro
+  SinricProSwitch &mySwitch1 = SinricPro[Abajour_ID];
+  mySwitch1.onPowerState(AbajourState);
+ 
+  SinricProSwitch &mySwitch2 = SinricPro[Lampada_ID];
+  mySwitch2.onPowerState(LampadaState);
+ 
+  SinricProSwitch &mySwitch3 = SinricPro[Regua_ID];
+  mySwitch3.onPowerState(ReguaState);
+ 
+  SinricProSwitch &mySwitch4 = SinricPro[Ventilador_ID];
+  mySwitch4.onPowerState(VentiladorState);
+ 
+  // setup SinricPro
+  SinricPro.onConnected([](){  }); 
+  SinricPro.onDisconnected([](){  });
+  SinricPro.begin(APP_KEY, APP_SECRET);
+}
